4cff293aa811a6e3cb8f225db9efcb1f177ce531
Michele da Rosa <misha.atelie@gmail.com>
2024-01-27 11:47:30 -0300

    greeting adicionado

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
-const resultArtist = document.getElementById("result-artist");
-const playlistContainer = document.getElementById("result-playlists");
-const searchInput = document.getElementById("search-input");
+//BOM DIA | BOA TARDE | BOA NOITE
 
-function requestApi(searchTerm) {
-    fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
-        .then((response) => response.json())
-        .then((results) => displayResults(results));
-}
+// Obtém a referência do elemento com o ID "greeting"
+const greetingElement = document.getElementById("greeting");
 
-function displayResults(results) {
-    hidePlaylists();
-    const artistImage = document.getElementById("artist-img");
-    const artistName = document.getElementById("artist-name");
+// Obtém a hora atual do sistema
+const currentHour = new Date().getHours();
 
-    results.forEach((element) => {
-        artistImage.src = element.urlImg;
-        artistName.innerText = element.name;
+// Define a saudação com base na hora atual
+// if (currentHour >= 5 && currentHour < 12) {
+//   greetingElement.textContent = "Bom dia";
+// } else if (currentHour >= 12 && currentHour < 18) {
+//   greetingElement.textContent = "Boa tarde";
+// } else {
+//   greetingElement.textContent = "Boa noite";
+// }
+
+// Forma mais simples
+const greetingMessage =
+    currentHour >= 5 && currentHour < 12 ?
+    "Bom dia ☀️" :
+    currentHour >= 12 && currentHour < 18 ?
+    "Boa tarde ⛅" :
+    "Boa noite 🌙";
+
+greetingElement.textContent = greetingMessage;
+
+// GRID INTELIGENTE
+const container = document.querySelector(".offer__list-item");
+
+const observer = new ResizeObserver(() => { //mudanças no tamanho do elemento 
+    const containerWidth = container.offsetWidth; //largura total do elemento, incluindo largura do conteúdo, bordas e preenchimento.
+    const numColumns = Math.floor(containerWidth / 200); //número de colunas com base na largura do container
+
+    //largura mínima de 200px e máxima de 1fr (uma fração do espaço disponível).
+    container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
+
+    console.log({
+        container
     });
-    resultArtist.classList.remove("hidden");
-}
-
-function hidePlaylists() {
-    playlistContainer.classList.add("hidden");
-}
-
-searchInput.addEventListener("input", function () {
-    const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === "") {
-        resultArtist.classList.add("hidden");
-        playlistContainer.classList.remove("hidden");
-        return;
-    }
-    requestApi(searchTerm);
+    console.log({
+        numColumns
+    });
 });
+//observando a mudança do elemento
+observer.observe(container);
